18c1ee14fd0c91efc1e4619f734d085de91df335
sanskarchoudhry <sanskar22101@gmail.com>
2025-06-26 23:55:02 +0530

    fix: moves change with pokemon variety(forms) change

--- src/pages/pokemon/PokemonPage.tsx
+++ src/pages/pokemon/PokemonPage.tsx
@@ -1,14 +1,28 @@
 import { useParams } from "react-router-dom";
 import Banner from "../../components/Banner";
 import PokemonDetails from "./_components/PokemonDetails";
+import { useEffect, useState } from "react";
+import {
+  fetchPokemonData,
+  PokemonData,
+} from "../../services/api/pokemonService";
 
 export default function PokemonPage() {
   const { id } = useParams();
+  const [data, setData] = useState<PokemonData>();
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetchPokemonData(id ?? " ");
+      setData(response);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <main className="flex flex-col justify-center items-center bg-[url(/assets/images/bg-pattern.jpg)]">
       <Banner />
-      {id && <PokemonDetails />}
+      {data && <PokemonDetails pokemonName={data.name} />}
     </main>
   );
 }
--- src/pages/pokemon/_components/PokemonDetails.tsx
+++ src/pages/pokemon/_components/PokemonDetails.tsx
@@ -1,62 +1,82 @@
-import { useParams } from "react-router-dom";
-import { useState } from "react";
-
-import { usePokemonDetails } from "../../../hooks/usePokemonDetails";
-
+import { useEffect, useState } from "react";
+import {
+  fetchPokemonData,
+  fetchPokemonSpeciesData,
+  PokemonData,
+  PokemonSpeciesData,
+} from "../../../services/api/pokemonService";
 import StatsWrapper from "./StatsWrapper";
 import MovesWrapper from "./MovesWrapper";
 import PokemonInfo from "./PokemonInfo";
 import EvolutionChain from "./EvolutionChain";
+import { useParams } from "react-router-dom";
 
-export default function PokemonDetails() {
-  const [selectedPokemonVariant, setSelectedPokemonVariant] = useState<
-    string | null
-  >(null);
-
+function PokemonDetails({ pokemonName }: { pokemonName: string }) {
   const { id } = useParams();
+  const [pokemonData, setPokemonData] = useState<PokemonData>();
+  const [pokemonSpeciesData, setPokemonSpeciesData] =
+    useState<PokemonSpeciesData>();
+  const [selectedPokemonVariant, setSelectedPokemonVariant] = useState<string>(
+    () => pokemonName
+  );
 
-  const { pokemonData, speciesData } = usePokemonDetails(id ?? " ");
+  useEffect(() => {
+    async function getSpeciesData() {
+      const response = await fetchPokemonSpeciesData(id ?? " ");
+      setPokemonSpeciesData(response);
+    }
 
-  const currentVariant = selectedPokemonVariant ?? pokemonData?.name;
+    async function getPokemonData() {
+      const response = await fetchPokemonData(selectedPokemonVariant);
+      setPokemonData(response);
+    }
 
-  if (!speciesData) {
-    return <div>Species</div>;
-  }
+    getSpeciesData();
+    getPokemonData();
+  }, [selectedPokemonVariant]);
 
-  if (!pokemonData) return <div>Pokemon Data</div>;
-  if (speciesData && pokemonData)
-    return (
-      <div className="flex flex-col gap-4 pt-8 p-16 w-[75%] bg-white mt-12 rounded-t-[20px]">
-        <h1 className="font-bold text-4xl capitalize text-center">
-          {speciesData.name}
-        </h1>
+  const handlePokemonVariantChange = (pokeName: string) => {
+    setSelectedPokemonVariant(pokeName);
+  };
 
-        <ul className="flex gap-2 border-b border-b-gray-primary/20">
-          {speciesData?.varieties?.length > 1 &&
-            speciesData.varieties.map((variant, index) => (
+  return (
+    <div className="flex flex-col gap-4 pt-8 p-16 w-[75%] bg-white mt-12 rounded-t-[20px] ">
+      <h1 className="font-bold text-4xl capitalize text-center">
+        {pokemonSpeciesData?.name}
+      </h1>
+      <ul className="flex gap-2 border-b border-b-gray-primary/20">
+        {pokemonSpeciesData?.varieties &&
+          pokemonSpeciesData?.varieties?.length > 1 &&
+          pokemonSpeciesData?.varieties?.map((pokemon, index) => {
+            return (
               <li
+                onClick={() => {
+                  handlePokemonVariantChange(pokemon.pokemon.name);
+                }}
                 key={index}
-                onClick={() => setSelectedPokemonVariant(variant.pokemon.name)}
                 className={`cursor-pointer p-1 px-2 rounded-t-[8px] border-[0.1px] border-gray-primary/20 border-b-white ${
-                  variant.pokemon.name === currentVariant
+                  pokemon.pokemon.name === selectedPokemonVariant
                     ? "bg-white"
                     : "bg-light-grey"
-                }`}
+                } `}
               >
-                {variant.pokemon.name}
+                {pokemon.pokemon.name}
               </li>
-            ))}
-        </ul>
+            );
+          })}
+      </ul>
+      <PokemonInfo pokemonName={selectedPokemonVariant} />
 
-        <PokemonInfo pokemonName={currentVariant!} />
-        <StatsWrapper pokemonName={currentVariant!} />
-        <EvolutionChain />
-        {pokemonData.moves && speciesData.generation?.url && (
-          <MovesWrapper
-            pokeMoves={pokemonData.moves}
-            pokemonGeneration={speciesData.generation.url}
-          />
-        )}
-      </div>
-    );
+      <StatsWrapper pokemonName={selectedPokemonVariant} />
+      <EvolutionChain />
+      {pokemonData && pokemonSpeciesData && (
+        <MovesWrapper
+          pokeMoves={pokemonData.moves}
+          pokemonGeneration={pokemonSpeciesData?.generation?.url}
+        />
+      )}
+    </div>
+  );
 }
+
+export default PokemonDetails;
