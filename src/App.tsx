67421a2b8f0d3c6e15bf4f2edcb9e892120660bc
sanskarchoudhry <sanskar22101@gmail.com>
2025-06-27 23:58:18 +0530

    feat: add moves page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home/HomePage";
 import PokemonPage from "./pages/pokemon/PokemonPage";
+import MovePage from "./pages/move/MovePage";
 
 function App() {
   return (
@@ -9,6 +10,10 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/pokemon/:id?" element={<PokemonPage />} />
+          <Route path="/move/" element={<MovePage />} />
+          <Route path="/move/generation/:genID?" />
+
+          <Route path="/move/:moveName?" />
         </Routes>
       </Router>
     </>
